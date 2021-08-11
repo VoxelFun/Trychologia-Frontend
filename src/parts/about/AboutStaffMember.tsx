1eac87f9ce3f2ec91d2ee22e60e9d39fc406a6c7
VoxelFun <padolod@gmail.com>
2021-08-11 04:04:28 +0200

    Add meta tags

--- src/parts/about/AboutStaffMember.tsx
+++ src/parts/about/AboutStaffMember.tsx
@@ -3,12 +3,13 @@ import {Image, Button} from "react-bootstrap";
 import { SafeStaffMember } from "../../library/model/SafeStaffMember";
 import { Card } from "../styled/Card";
 import { SectionContainer } from "../main-page/Section";
+import { RouteComponentProps, withRouter } from "react-router";
 
-interface Props {
+interface Props extends RouteComponentProps<any> {
     staffMember: SafeStaffMember;
 }
 
-export default function AboutStaffMember(props: Props) {
+function AboutStaffMember(props: Props) {
     const {staffMember} = props;
     return (
         <SectionContainer>
@@ -21,7 +22,7 @@ export default function AboutStaffMember(props: Props) {
                     {staffMember.description}
                 </Card.Body>
                 <Card.Footer>
-                    <Button>
+                    <Button onClick={() => props.history.replace("/wizyta")}>
                         Umów wizytę
                     </Button>
                 </Card.Footer>
@@ -41,3 +42,5 @@ export default function AboutStaffMember(props: Props) {
 const Photo = styled(Image)`
 
 `;
+
+export default withRouter(AboutStaffMember);
